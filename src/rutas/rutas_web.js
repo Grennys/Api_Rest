758e157db2cc468615cebc096853a11d09d45f9f
Grennys <greni00salgado@gmail.com>
2022-09-05 12:58:30 -0500

    Ruta eliminar productos agregada

--- src/rutas/rutas_web.js
+++ src/rutas/rutas_web.js
@@ -8,10 +8,17 @@ const myData = JSON.parse (data);
 //Libreria que nos falicita el manejo de los datos
 const under = require('underscore');
 
+let Eprodcuto = myData;
 
 
-
-
+//Eliminar un producto
+rutas.delete('/productos/:sku', (req, res ) => {
+    const { sku } = req.params;
+    Eprodcuto = Eprodcuto.filter(Eprodcuto => Eprodcuto.sku != req.params.sku);
+    const nuevoP_json = JSON.stringify(Eprodcuto);
+    fs.writeFileSync('src/Base_datos/productos.json', nuevoP_json);
+    res.send('¡Se eliminó el producto satisfactoriamente!');
+});
 
 
 module.exports = rutas;
