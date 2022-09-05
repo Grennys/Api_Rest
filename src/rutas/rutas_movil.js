0471a581a8a8811ab61e9174da0c767312a579ba
Grennys <greni00salgado@gmail.com>
2022-09-05 13:29:53 -0500

    Ruta para movil resumen compra agregada

--- src/rutas/rutas_movil.js
+++ src/rutas/rutas_movil.js
@@ -9,7 +9,27 @@ const myData = JSON.parse (data);
 const under = require('underscore');
 
 
+//Resumen de compra
+rutas.post('/resumen', (req, res ) => {      
+    
+    let ArrayResumen = [];    
+    let precio_final = 0;
+    let total_compra = 0;
 
+    under.each(myData, (producto,i) =>{    
+        under.each(req.body, (sku, i) => {
+            if(producto.sku == sku){                   
+                precio_final = (producto.precio - (producto.precio * producto.descuento) + (producto.precio + producto.iva));
+                total_compra =  precio_final + total_compra;            
+                ArrayResumen.push({
+                    "sku:": producto.sku, "Nombre: ": producto.nombre, "Precio_final": precio_final,"total_compra: ": total_compra            
+                });                         
+            }                          
+        });
+    });         
+    res.json(ArrayResumen);
+    
+});
 
 
 module.exports = rutas;
