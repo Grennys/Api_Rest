9002526a24170e134e1fc1d52d08a30211829677
Grennys <greni00salgado@gmail.com>
2022-09-05 13:21:58 -0500

    Ruta para movil consultar producto agregada

--- src/rutas/rutas_movil.js
+++ src/rutas/rutas_movil.js
@@ -8,7 +8,25 @@ const myData = JSON.parse (data);
 //Libreria que nos falicita el manejo de los datos
 const under = require('underscore');
 
+//Consultar productos
+rutas.get('/producto/:sku', (req, res) =>{            
+    const { sku } = req.params;    
+    const dataString = JSON.stringify(myData);          
+    let array =  JSON.parse(dataString);    
+    var ArrayData = [];        
 
+    under.each(array, (producto,i) =>{     
+        precio_final = (producto.precio - (producto.precio * producto.descuento) + (producto.precio + producto.iva));        
+        if(producto.sku == sku){
+            ArrayData.push({
+                "sku: ": producto.sku,"Nombre: ": producto.nombre, "Precio: ": producto.precio, 
+                "Url: ": producto.url, "Marca: ": producto.marca, "Descripción: ": producto.descripcion,
+                "Iva: ": producto.iva, "Desceunto: ": producto.descuento,"precio final: ": precio_final
+            });
+        }        
+    });                  
+    res.json(ArrayData);        
+});
 
 
 
