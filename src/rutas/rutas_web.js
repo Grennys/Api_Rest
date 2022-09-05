ed5aee3f0a25192d2c9456f1cd5d486db0abffa2
Grennys <greni00salgado@gmail.com>
2022-09-05 13:04:17 -0500

    Ruta actualizar productos agregada

--- src/rutas/rutas_web.js
+++ src/rutas/rutas_web.js
@@ -10,7 +10,45 @@ const under = require('underscore');
 
 
 
+//Ruta para actulizar 
+rutas.put('/productos/:id', (req, res ) => {   
+    let Nprodcuto = myData;    
+    const { id } = req.params;                
+    var date = new Date(); 
+    var fecha = date.toLocaleDateString();       
+    const { sku, nombre,precio,url,marca,descripcion,iva,descuento,inventario } = req.body;    
+    if ( sku && nombre && precio && url && marca && descripcion && iva && descuento && inventario ) {                
+        under.each(myData, (producto,i) =>{  
+            console.log(id, producto.sku);
+            if(producto.sku == id){                                 
+                Nprodcuto = Nprodcuto.filter(Nprodcuto => Nprodcuto.sku != req.params.id);
+                const nuevoP_json = JSON.stringify(Nprodcuto);
+                fs.writeFileSync('src/Base_datos/productos.json', nuevoP_json);
+            }                      
+        });  
 
+        let NuevoP = {
+            sku, 
+            nombre,
+            precio,
+            url,
+            marca,
+            descripcion,
+            iva,
+            descuento,
+            inventario,
+            fecha
+        };
+        Nprodcuto.push(NuevoP);
+        const nuevoP_json =  JSON.stringify(Nprodcuto);
+        fs.writeFileSync('src/Base_datos/productos.json', nuevoP_json, 'utf-8');{
+            res.send('El producto se actualizo satisfactoriamente');
+        }             
+
+    } else {
+        res.status(500).json({error: 'Todos los campos son requeridos.'});
+    }           
+});
 
 
 
