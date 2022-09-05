ab18ed01a89a86e380ca8cec5a41ebdc6964b8f5
Grennys <greni00salgado@gmail.com>
2022-09-05 13:07:35 -0500

    Ruta ordenes de compra agregada

--- src/rutas/rutas_web.js
+++ src/rutas/rutas_web.js
@@ -12,7 +12,26 @@ const under = require('underscore');
 
 
 
+//Ordenes de compra
+rutas.get('/ordenes', (req, res) =>{       
+    const fs = require('fs');    
+    const data = fs.readFileSync('src/Base_datos/ordenes.json');
+    const myOrdenes = JSON.parse (data);                  
+    
+    let arrayCompra = [];    
 
+    under.each(myOrdenes, (orden,i) =>{            
+        under.each(myData, (producto, j) => {
+            if(orden.productos[i] = producto.sku  ){       
+                arrayCompra.push({
+                    orden,"Nombre":producto.nombre, "Sku":producto.sku, "marca":producto.marca,"url": producto.url    
+                }); 
+            }                          
+        });        
+    });          
+    res.json(arrayCompra);
+    
+});
 
 module.exports = rutas;
 
